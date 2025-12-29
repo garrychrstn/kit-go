d2fb422e5c703b62d8272c4f9ba859712965fc52
garrychrstn <garrychrstn@gmail.com>
2025-12-29 20:38:20 +0700

    add user verification

--- src/types/t.auth.go
+++ src/types/t.auth.go
@@ -3,6 +3,7 @@ package types
 type IRequestLogin struct {
 	UsernameOrEmail string `json:"username_or_email" binding:"required"`
 	Password        string `json:"password" binding:"required"`
+	Age             int    `json:"age" binding:"required,gt=10,max=150"`
 }
 
 type IPartUser struct {
